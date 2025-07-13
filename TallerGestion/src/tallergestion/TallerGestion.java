01e424b53c990bf920daa282a4f1c42f4261d72b
Aleida Rivera <aleidarivera306@gmail.com>
2025-07-13 13:04:38 -0600

    Actualizacion - va de la mano con la documentacion, Modulo Cliente en proceso

---
+++ TallerGestion/src/tallergestion/TallerGestion.java
@@ -0,0 +1,13 @@
+package tallergestion;
+
+import javax.swing.SwingUtilities;
+
+public class TallerGestion {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Vista vista = new Vista(); 
+            vista.setVisible(true);    //muestra mi pantalla principal, en este caso "vista"
+        });
+    }
+}
